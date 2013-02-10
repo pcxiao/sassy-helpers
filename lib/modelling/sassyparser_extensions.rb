29fe6c2eaaa7201caa5d1880c1466691864a9edf
Peter Krusche <pkrusche@gmail.com>
2013-02-10 14:18:23 +0000

    Equation parsing

--- lib/modelling/sassyparser_extensions.rb
+++ lib/modelling/sassyparser_extensions.rb
@@ -9,10 +9,23 @@ module Modelling
 				if fun[:parameters].length != 3
 					raise "Model function must get exactly tree parameters"
 				end
+				if fun[:returns][0] != d[:dd]
+					raise "Return assignment is invalid"
+				end
 				if eval.length != 1 || eval[0] != fun[:parameters][2]
 					raise("Invalid eval statement")
 				end
 			end
 		end
+
+		class Equation < Treetop::Runtime::SyntaxNode
+			attr_accessor :equation
+			attr_accessor :comments
+
+			def initialize
+				@equation = ""
+				@comments = ""
+				super
+			end
 	end
 end
