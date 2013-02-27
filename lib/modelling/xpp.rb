01b35c790e8c17694579ef5f3fdb435a1598a11d
Peter Krusche <pkrusche@gmail.com>
2013-02-27 12:44:00 +0000

    Removed unsupported ode files, fixed assignment list import in xpp

--- lib/modelling/xpp.rb
+++ lib/modelling/xpp.rb
@@ -4,18 +4,18 @@ module Modelling
 
 	# XPP in/output
 	module XPPModel
-		
+
 		# read an xpp file
 		def from_xpp(filename)
 			lines = []
 			current_line = ""
 			cont = false
 			# handle line continuations
-			File.foreach(filename) do |line| 
+			File.foreach(filename) do |line|
 				if line.match(/^(.*)\\\s*$/)
 					current_line= current_line + $1
 					cont = true
-				else  
+				else
 					current_line= current_line + line
 					cont = false
 				end
@@ -36,7 +36,7 @@ module Modelling
 			lines.each do |l|
 				# xpp options -- we don't really know what to do with them but save
 				# them in case someone else does
-				if l.match(/^\@/)
+				if l.match(/^[\"\@]/)
 					@xpp_extra.push l unless l == ""
 					if not comment_used and last_comment != ""
 						if @notes == ""
@@ -61,7 +61,7 @@ module Modelling
 				# parameters or numbers
 				if l.match(/^[pn][a-zA-Z]*\s+([a-zA-Z].*)$/)
 					ll = $1
-					ps = ll.split(/\s*,\s*/)
+					ps = _split_assignment_list(ll)
 					ps.each do |pl|
 						if pl.strip.match(/([a-zA-Z\_][a-zA-Z\_0-9]*)\s*\=\s*([0-9eE+\-\.]+)/)
 							@parameters[$1] = Parameter.new($1, $2.to_f, last_comment)
@@ -74,6 +74,19 @@ module Modelling
 					end
 					comment_used = true
 				elsif l.match(%r(d([a-zA-Z0-9\_]+)/dt\s*\=(.*)))
+					pname = $1
+					eqn = $2
+
+					if !@species[pname]
+						@species[pname] = Species.new(pname, 0.0)
+					end
+
+					@rules.push Rule.new(@species[pname], Equation.new(eqn, last_comment), 'rate')
+					comment_used = true
+
+			#		puts "dxdt:  (d#{@species[pname].to_s}/dt <- #{eqn})\n"
+				# species rate rule of the form x' = ...
+				elsif l.match(%r(([a-zA-Z0-9\_]+)'\s*\=(.*)))
 					pname = $1
 					eqn = $2
 
@@ -98,7 +111,7 @@ module Modelling
 				# initial value assignment
 				elsif l.match(/^i[a-zA-Z]*\s+([a-zA-Z0-9\_]+\s*\=.*)$/)
 					ll = $1
-					ps = ll.split(/\s*,\s*/)
+					ps = _split_assignment_list(ll)
 					ps.each do |pl|
 						if pl.strip.match(/([a-zA-Z\_][a-zA-Z\_0-9]*)\s*\=\s*([0-9eE\+\-\.]+)/)
 							@species[$1] = Species.new($1, $2.to_f)
@@ -144,7 +157,7 @@ module Modelling
 			no = 1
 			@species.each do |n, s|
 				s.matlab_no=(no)
-				no = no + 1				
+				no = no + 1
 			end
 
 			# fix rule outputs
@@ -156,14 +169,99 @@ module Modelling
 				else
 					raise "invalid rule #{r} \n"
 				end
-						
+
 			end
 
 			self.validate
 		end
 
+		# write an xpp file
 		def to_xpp(filename)
-			
+			if not filename.match(%r(.ode$))
+				filename = filename + ".ode"
+			end
+			xextra = ""
+			if @xpp_extra
+				xextra = @xpp_extra.join("\n")
+			end
+			File.open("#{filename}", "w") do |f|
+				f.puts <<-END
+# #{@notes.gsub("%%%%-cb-%%%%", "\n# ")}
+
+#{xextra}
+
+END
+				f.puts("# Parameters \n")
+				f.puts("# ---------- \n\n")
+				@parameters.each do |n,par|
+					if par.description
+						pcc = par.description.gsub("%%%%-cb-%%%%", "\n# ")
+						f.puts("# #{pcc}\n")
+					end
+					f.puts("par #{par.name} = #{par.value}\n")
+				end
+				f.puts("\n")
+				f.puts("# Initial values \n")
+				f.puts("# -------------- \n\n")
+				@species.each do |n,spec|
+					f.puts("# y(#{spec.matlab_no}) \n")
+					f.puts("init #{spec.name} = #{spec.initial}\n")
+				end
+				f.puts("\n")
+				f.puts("# Rules \n")
+				f.puts("# ----- \n\n")
+				@rules.each do |r|
+					eqc = r.equation.comments.gsub("%%%%-cb-%%%%", "\n# ")
+					f.puts "# #{eqc}\n"
+					f.puts "#{r.to_s}\n\n"
+				end
+			end
+		end
+
+		# split a a list of assignments
+		#
+		# Input is a string of assignments like `a = 1 b = 2, c = 3`
+		# (i.e. we can have varying whitespace and optional commas)
+		def _split_assignment_list(lstr)
+			tokens = lstr.split(/[\s,;\n\r]+/)
+			current = ""
+			state = "n"
+			ret = []
+			tokens.each do |tok|
+				case state
+				when "n"
+					if tok.match(/\=[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?$/)
+						ret.push "#{tok}"
+						current = ""
+					elsif tok.match(/\=$/)
+						current = "#{tok}="
+						state = "v"
+					else
+						current = "#{tok}"
+						state = "="
+					end
+				when "="
+					if not tok.match(/^\=/)
+						raise "invalid assignment list, expected '=', but got '#{tok}'"
+					else
+						current = current + tok
+					end
+					if tok.match(/\=[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?$/)
+						state = "n"
+						ret.push(current + tok)
+						current = ""
+					else
+						state = "v"
+					end
+				when "v"
+					ret.push(current + tok)
+					current = ""
+					state = "n"
+				else
+					raise "internal parsing error: unknown state #{state}"
+				end
+			end
+			ret
 		end
 
 	end
--- spec/xpp_spec.rb
+++ spec/xpp_spec.rb
@@ -4,10 +4,37 @@ require "sassy-helpers"
 require 'tempfile'
 
 describe Modelling::XPPModel do
-	it 'loads xpp models' do 
+	it 'loads xpp models' do
 		m = Modelling::Model.new
 		m.from_xpp('spec/testmodels/ode/cellcycle_gerard10_5var.ode')
 		m.species.length.should eql(5)
 		m.parameters.length.should eql(24)
 	end
+
+	it 'writes xpp models' do
+		m = Modelling::Model.new
+		m.from_sassy('spec/testmodels/sassy/mammalian')
+
+		# make a temp file
+		file1 = Tempfile.new('model_tmp')
+		p = file1.path
+		file1.close
+		file1.unlink   # deletes the temp file
+		
+		# write an ode file
+		m.to_xpp("#{p}.ode")
+
+		modeltext = File.read("#{p}.ode").gsub("\r", "")
+		modeltext.should eql(File.read("spec/testmodels/ode/mammalian.ode").gsub("\r", ""))
+	end
+
+	it 'parses various ode files' do
+		files = Dir["spec/testmodels/ode/*.ode"]
+		files.each do |variable|
+			m = Modelling::Model.new
+			m.from_xpp(variable)
+			m.species.length.should be > 0
+		end
+	end
+
 end
