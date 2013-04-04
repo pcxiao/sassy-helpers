b0a546dc35d525f57c9025d5af411672799695af
Peter Krusche <pkrusche@gmail.com>
2013-04-04 16:24:01 +0100

    SBML export working

--- lib/modelling/sbml.rb
+++ lib/modelling/sbml.rb
@@ -12,9 +12,75 @@ module Modelling
 					"level" => "1",
 					"version" => "2"
 				})
+
+				model = REXML::Element.new("model")
+				model.add_attribute('name', 'Exported_Model')
 				
-				doc.add_element top
-				formatter = REXML::Formatters::Default.new( 5 )
+				if @notes.length > 0
+					nel = REXML::Elements.new("notes")
+					nel << REXML::Text.new("#{@notes.gsub("%%%%-cb-%%%%", "\n# ")}")
+					model << nel
+				end
+
+				lc = REXML::Element.new "listOfCompartments"
+				cm = REXML::Element.new "compartment"
+				cm.add_attributes({ "name" => "cell", "volume" => "1"})
+				lc << cm
+				model << lc
+
+				ls = REXML::Element.new "listOfSpecies"
+				@species.each do |n,spec|
+					s = REXML::Element.new "species"
+					pcc = "y(#{spec.matlab_no})"
+					s << REXML::Comment.new(pcc)
+					s.add_attributes({
+						"id" => spec.name,
+						"initialAmount" => spec.initial,
+						"compartment" => "cell",
+						"boundaryCondition" => "true"
+						})
+					ls << s
+				end
+				model << ls
+
+				lp = REXML::Element.new "listOfParameters"
+				@parameters.each do |n,par|
+					p = REXML::Element.new "parameter"
+					if par.description
+						pcc = par.description.gsub("%%%%-cb-%%%%", "\n# ")
+						p << REXML::Comment.new(pcc)
+					end
+					p.add_attributes({
+						"name" => par.name,
+						"value" => par.value
+						})
+					lp << p
+				end
+				model << lp
+
+				lr = REXML::Element.new "listOfRules"
+				@rules.each do |r|
+					rt = "speciesConcentrationRule"
+					re = REXML::Element.new rt
+					eqc = r.equation.comments.gsub("%%%%-cb-%%%%", "\n# ")
+					re << REXML::Comment.new(eqc)
+					re.add_attributes({
+						"species" => r.output.name,
+						"type" => r.type,
+						"formula" => r.equation.to_s,
+						"compartment" => "cell"
+						})
+					lr << re
+				end
+				model << lr
+
+				top << model
+
+				doc << REXML::XMLDecl.new(1.0, "utf-8")
+				doc << top
+
+				# Write to file
+				formatter = REXML::Formatters::Pretty.new(5)
 				formatter.write(doc, file)
     		end
 		end
--- spec/libsbml_spec.rb
+++ spec/libsbml_spec.rb
@@ -17,7 +17,7 @@ describe Modelling::SBMLModel do
 		m1.add_species('X')
 		m1.add_parameter('k1_x', 1)
 		m1.add_parameter('k2_x', 1)
-		m1.add_rule('X', 'k1_x*X - k2_x*X')
+		m1.add_rule('X', 'k1_x*X - k2_x*X', 'rate')
 
 		file1 = Tempfile.new('model_tmp')
 		p = file1.path
@@ -28,6 +28,6 @@ describe Modelling::SBMLModel do
 		m1.to_sbml("#{p}.xml")
 
 		modeltext = File.read("#{p}.xml").gsub("\r", "")
-		p modeltext
+		modeltext.should eql("<?xml version='1.0' encoding='UTF-8'?>\n<sbml xmlns='http://www.sbml.org/sbml/level1' level='1' version='2'>\n     <model name='Exported_Model'>\n          <listOfCompartments>\n               <compartment name='cell' volume='1'/>\n          </listOfCompartments>\n          <listOfSpecies>\n               <species id='X' initialAmount='0' compartment='cell' boundaryCondition='true'>\n                    <!--y(2)-->\n               </species>\n          </listOfSpecies>\n          <listOfParameters>\n               <parameter name='k1_x' value='1.0'>\n                    <!---->\n               </parameter>\n               <parameter name='k2_x' value='1.0'>\n                    <!---->\n               </parameter>\n          </listOfParameters>\n          <listOfRules>\n               <speciesConcentrationRule species='X' type='rate' formula='k1_x*X - k2_x*X' compartment='cell'>\n                    <!---->\n               </speciesConcentrationRule>\n          </listOfRules>\n     </model>\n</sbml>")
 	end
 end
